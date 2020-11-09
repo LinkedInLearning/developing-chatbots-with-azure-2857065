9a754b050da61bd8f70d07f61b14253b4416e5af
bhavaniravi <bhava0895@gmail.com>
2020-11-09 09:40:19 +0530

    update datafetcher to get jobs

--- redtech30_job_bot/data_fetcher.py
+++ redtech30_job_bot/data_fetcher.py
@@ -7,16 +7,10 @@ class JobDataFetcher:
     def __init__(self):
         self.dataframe = pd.read_csv(JOBS_DATA_PATH)
 
-    def get_job(self, name):
-        name = name.lower()
-        return self.dataframe[self.dataframe["Title"].str.contains(name, case=False)]
-
-    def get_product_info(self, name):
-        return self.get_product(name)["Job description"].iloc[0]
-
-    def get_application_link(self, name):
-        return self.get_product(name)["Where to apply"].iloc[0]
-
-    def get_jobs_by_skill(self, skill):
-        skill = skill.lower()
-        return self.dataframe[self.dataframe["Technology"].str.contains(skill, case=False)]
+    def get_job(self, technology=None, years=None, location=None):
+        technology = ", ".join(technology) if technology else ""
+        location = ", ".join(location) if location else ""
+        years = int(years[0])
+        return self.dataframe[(self.dataframe["Technology"].str.contains(technology, case=False)) & 
+                              (self.dataframe["Years of experience"] <= years) &
+                              (self.dataframe["Location"].str.contains(location, case=False))  ]
